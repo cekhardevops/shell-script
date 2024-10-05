565f9192f6b320e3b209bf8f322c9e5955488322
cekhardevops <devsecmlops@gmail.com>
2024-10-05 16:40:38 +0530

    01-script

---
+++ conditions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# if [ NUMBER>10 ]; then
+#     # Code to run if condition is true
+# elif [ another_condition ]; then
+#     # Code to run if another_condition is true
+# else
+#     # Code to run if no conditions are true
+# fi
+
+# Operator	Meaning
+# -eq	Equal to
+# -ne	Not equal to
+# -lt	Less than
+# -le	Less than or equal to
+# -gt	Greater than
+# -ge	Greater than or equal to
+
+
+if [ $NUMBER -gt 20 ]; then
+    echo "given number $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "given number $NUMBER is equal to 20"
+else
+    echo "given number $NUMBER is less than 20"
