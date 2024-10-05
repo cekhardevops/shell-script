ddd683944f8f1d9139ce0fc8da10bdf8c61fb5dd
cekhardevops <devsecmlops@gmail.com>
2024-10-05 16:09:21 +0530

    01-script

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -7,6 +7,6 @@ echo "current working directory: ${PWD}"
 echo "first second last parameter pass to the script : $1 $2 ${!#}"
 echo "PID of the script executing now: $$"
 ls /nonexistent-directory
-echo "exit status of previous command $?
-echo hello
+echo "exit status of previous command $?"
+echo "hello"
 echo "last argument of previous command $_
