20b3aeb9b5c866b6895d42e46a412f478e4e3c93
cekhardevops <devsecmlops@gmail.com>
2024-10-05 16:27:34 +0530

    01-script

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -4,7 +4,7 @@ echo "All the variables passed to this script : ${@}"
 echo "Number of variables passed to this script : ${#}"
 echo "script name : ${0}"
 echo "current working directory: ${PWD}"
-echo "Use's Home directory : ${HOME}
+echo "Use's Home directory : ${HOME}"
 eval last_arg=\$$#
 echo "first second last parameter pass to the script : $1 $2 $last_arg"
 echo "PID of the script executing now: $$"
