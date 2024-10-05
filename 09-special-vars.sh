2f523040149e0b39085d94a801e5097380af0049
cekhardevops <devsecmlops@gmail.com>
2024-10-05 16:22:48 +0530

    01-script

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -4,7 +4,8 @@ echo "All the variables passed to this script : ${@}"
 echo "Number of variables passed to this script : ${#}"
 echo "script name : ${0}"
 echo "current working directory: ${PWD}"
-echo "first second last parameter pass to the script : $1 $2 \$$$#"
+eval last_arg=\$$#
+echo "first second last parameter pass to the script : $1 $2 $last_arg"
 echo "PID of the script executing now: $$"
 ls /nonexistent-directory
 echo "exit status of previous command $?"
