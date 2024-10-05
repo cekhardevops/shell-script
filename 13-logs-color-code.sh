ab6d5bc3226732c7bf8e4a247b6c8a1d498e29eb
cekhardevops <devsecmlops@gmail.com>
2024-10-05 21:14:31 +0530

    01-script

--- 13-logs-color-code.sh
+++ 13-logs-color-code.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+
+# # \033 or \e: Escape character.
+
+
+# Basic Styles:
+# Style Code	Description
+# 0	Reset/Normal
+# 1	Bold/Bright
+# 4	Underline
+# 7	Invert colors
+# Foreground Color Codes:
+# Color	Code
+# Black	30
+# Red	31
+# Green	32
+# Yellow	33
+# Blue	34
+# Magenta	35
+# Cyan	36
+# White	37
+
 log_info() {
     echo -e "\033[32m[INFO] $1\033[0m"   # Green for info
 }
---
+++ 14-functions-colors.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    log_error "please run the script with root previledges"
+    exit 1;
+fi
+
+<<comment
+validate_and_install funtion checks below steps
+4. check whether software is already installed. if it already installed, inform the user it is already installed
+5. if not , intall it.
+6. check it is success or not.
+
+comment
+
+validate_and_install(){
+    if [ $1 -ne 0 ]; then
+        echo "installing $2 ... "
+        dnf install $2 -y
+        if [ $? -ne 0 ]; then
+            log_error "$2 installation is failed ... please check it"
+            exit 1;
+        else
+            log_info "$2 installed successfully..."
+        fi
+    else
+        log_warning "$2 is already installed..... nothing to do."
+    fi
+}
+
+log_info() {
+    echo -e "\033[32m[INFO] $1\033[0m"   # Green for info
+}
+
+log_warning() {
+    echo -e "\033[33m[WARNING] $1\033[0m" # Yellow for warnings
+}
+
+log_error() {
+    echo -e "\033[31m[ERROR] $1\033[0m"   # Red for errors
+}
+
+
+dnf list installed mysql-server
+
+validate_and_install $? mysql-server
+
+dnf list installed nginx
+
+validate_and_install $? nginx
