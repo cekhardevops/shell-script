ff906b0519a84c9f46faae7f2ed03ba31806f4c2
cekhardevops <devsecmlops@gmail.com>
2024-10-05 15:58:16 +0530

    01-script

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -2,3 +2,6 @@
 
 echo "All the variables passed to this script : $@"
 echo "Number of variables passed to this script : $#"
+echo "script name : $0"
+echo "current working directory: $PWD"
+echo "first second last parameter pass to the script : $1 $2 $N"
