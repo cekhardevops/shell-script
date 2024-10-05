266e79338f568c685ba57ffd6b65ee45628bfec6
cekhardevops <devsecmlops@gmail.com>
2024-10-05 16:43:42 +0530

    01-script

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -24,6 +24,8 @@ if [ $NUMBER -gt 20 ]; then
     echo "given number $NUMBER is greater than 20"
 elif [ $NUMBER -eq 20 ]; then
     echo "given number $NUMBER is equal to 20"
-else
+elif [ $NUMBER -le 20 ]; then
     echo "given number $NUMBER is less than 20"
+else
+    echo "not a valid number"
 fi
