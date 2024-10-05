cf2366d274ec40382b0d2952012873196954d93c
cekhardevops <devsecmlops@gmail.com>
2024-10-05 16:03:46 +0530

    01-script

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "All the variables passed to this script : $@"
-echo "Number of variables passed to this script : $#"
-echo "script name : $0"
-echo "current working directory: $PWD"
-echo "first second last parameter pass to the script : $1 $2 $N"
+echo "All the variables passed to this script : ${@}"
+echo "Number of variables passed to this script : ${#}"
+echo "script name : ${0}"
+echo "current working directory: ${PWD}"
+echo "first second last parameter pass to the script : $1 $2 ${!#}"
+echo "PID of the script executing now: $$"
