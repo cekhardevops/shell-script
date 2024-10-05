fdb87f127c183b92f0c982252530250ce99ca85d
cekhardevops <devsecmlops@gmail.com>
2024-10-05 14:46:51 +0530

    01-script

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo " Hi ${person1}, how are you"
+
+echo "${person2}: Hello xyz, How are you doing"
+
+echo "${person1}: I am good abc, what's going on"
+
+echo "${person2}: I started learning shell scripting"
