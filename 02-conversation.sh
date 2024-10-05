c84e6454f78aadd09c87352b894d98c18d001a66
cekhardevops <devsecmlops@gmail.com>
2024-10-05 12:51:44 +0530

    01-script

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "xyz: Hi abc, how are you"
+
+echo "abc: Hello xyz, How are you doing"
+
+echo "xyz: I am good abc, what's going on"
+
+echo "abc: I started learning shell scripting"
