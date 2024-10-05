9199d539c4b471bb21489ffc4289c033bd4c6994
cekhardevops <devsecmlops@gmail.com>
2024-10-05 19:45:35 +0530

    01-script

---
+++ 11-install-software.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+<<comment
+
+steps to install software using shell
+1. check whether user has root access
+2. if user has root access, proceed with script
+3. other throw error
+4. check whether software is already installed. if it already installed, inform the user it is already installed
+5. if not , intall it.
+6. check it is success or not.
+
+comment
