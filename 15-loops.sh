a85f5075b238ff6ac59f07d027659c362af88c55
cekhardevops <devsecmlops@gmail.com>
2024-10-06 08:52:16 +0530

    01-script

--- 15-loops.sh
+++ 15-loops.sh
@@ -3,23 +3,23 @@
 for i in 1 2 3 4 5 6 7 8 9
 do 
     echo "num is $i"
-    echo "@@@@@@@@@@@@@@@@@@@@"
 done 
+    echo "@@@@@@@@@@@@@@@@@@@@"
 
 
 for i in {1..9}
 do
     echo "num is $i"
-    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@
 done
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 fetch list of files in the specified directory.
 for file_name in ${PWD}/*
 do
     echo "file name : $file_name"
-    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 done
 
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 list=("chandra" "sekhar" "reddy")
 
 for name in ${list[@]}
