5f6469fc7972c42ea2de5fe0318839e002f88250
cekhardevops <devsecmlops@gmail.com>
2024-10-06 18:59:54 +0530

    01-script

--- 19-delete-logs.sh
+++ 19-delete-logs.sh
@@ -10,6 +10,18 @@ Y=[33m
 R=[31m
 N=[0m
 
+log_info() {
+    echo -e "\033$G $(date +'%Y-%m-%d %H:%M:%S') [INFO] $1\033$N"  # Green for info
+}
+
+log_warning() {
+    echo -e "\033[33m $(date +'%Y-%m-%d %H:%M:%S') [WARNING] $1\033[0m"  # Yellow for warnings
+}
+
+log_error() {
+    echo -e "\033[7;31m $(date +'%Y-%m-%d %H:%M:%S') [ERROR] $1\033[0m"  # Red for errors
+}
+
 #\033 or \e we can use 
 
 # log_info() {
@@ -26,17 +38,17 @@ N=[0m
 
 
 
-log_info() {
-    echo -e "\033$G $(date +'%Y-%m-%d %H:%M:%S') [INFO] $1\033$N"  | tee -a "$LOG_FILE" # Green for info
-}
+# log_info() {
+#     echo -e "\033$G $(date +'%Y-%m-%d %H:%M:%S') [INFO] $1\033$N"  | tee -a "$LOG_FILE" # Green for info
+# }
 
-log_warning() {
-    echo -e "\033[33m $(date +'%Y-%m-%d %H:%M:%S') [WARNING] $1\033[0m" | tee -a "$LOG_FILE" # Yellow for warnings
-}
+# log_warning() {
+#     echo -e "\033[33m $(date +'%Y-%m-%d %H:%M:%S') [WARNING] $1\033[0m" | tee -a "$LOG_FILE" # Yellow for warnings
+# }
 
-log_error() {
-    echo -e "\033[7;31m $(date +'%Y-%m-%d %H:%M:%S') [ERROR] $1\033[0m" | tee -a "$LOG_FILE"  # Red for errors
-}
+# log_error() {
+#     echo -e "\033[7;31m $(date +'%Y-%m-%d %H:%M:%S') [ERROR] $1\033[0m" | tee -a "$LOG_FILE"  # Red for errors
+# }
 
 if [ -d $SOURCE_DIR ]; then
     log_info "source directory exists"
