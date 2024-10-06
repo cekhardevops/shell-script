091d8a6aa882460c394540a3268b056cbfdf4e56
cekhardevops <devsecmlops@gmail.com>
2024-10-06 09:21:06 +0530

    01-script

--- 16-install-packges-loops.sh
+++ 16-install-packges-loops.sh
@@ -8,5 +8,28 @@ validate_user(){
         exit 1;
     fi
 }
+validate_and_install(){
+
+    dnf list installed $1
+    if [ $? -ne 0 ]; then
+        echo "$1 is isntalling...."
+        dnf install $1 -y
+        if [ $? -ne 0 ]
+            echo "$1 package not installed. please check..."
+        else
+            echo "*****$1 package is installed successfully******"
+        fi
+    else
+        echo "$1 already isntalled...."
+    fi
+}
+
+
+
 
 validate_user $user_id
+
+for package in $@
+do
+    validate_and_install $package
+done
