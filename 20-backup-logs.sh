0fcb180675131b0daf6fa663f2e2e9ee847fb315
cekhardevops <devsecmlops@gmail.com>
2024-10-06 20:04:20 +0530

    01-script

--- 20-backup-logs.sh
+++ 20-backup-logs.sh
@@ -38,7 +38,7 @@ Files=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 if [ ! -z $Files]; then 
     log_info "Files are found"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip
-    find $SOURCE_DIR -name "*.log" -mtime +14 | zip "$ZIP_FILE -@
+    find $SOURCE_DIR -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
 
     if [ -f $ZIP_FILE]; then
         log_info "successfullhy zipped files older than $NO_OF_DAYS"
