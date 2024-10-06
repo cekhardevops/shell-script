da638a32dff30854d94de1e9fa69f43599c6d53b
cekhardevops <devsecmlops@gmail.com>
2024-10-06 18:19:35 +0530

    01-script

--- 18-set.sh
+++ 18-set.sh
@@ -6,3 +6,10 @@ echo "hello world"
 echhhhhhhh "sdfsdfsd"
 echo "after error"
 
+failure(){
+    echo "failed at : $1 $2
+}
+
+# trap function when executing script, it will give ERR signal
+
+trap 'failure "${LINENO}" ${BASH_COMMAND}' ERR
